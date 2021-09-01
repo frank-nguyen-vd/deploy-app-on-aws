4227d1888c7849f8e9d8b808fd7ee125b44c760f
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-09-01 22:31:10 +0800

    Upload files to S3 Bucket
    Upload to S3 Bucket

--- backend/src/controllers/file-upload.controller.ts
+++ backend/src/controllers/file-upload.controller.ts
@@ -14,6 +14,19 @@ import {
 import { FILE_UPLOAD_SERVICE } from "../keys";
 import { FileUploadHandler } from "../types";
 
+import dotenv from "dotenv";
+
+import AWS from "aws-sdk";
+
+import fs from "fs";
+
+dotenv.config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+});
+
 /**
  * A controller to handle file uploads using multipart/form-data media type
  */
@@ -60,6 +73,7 @@ export class FileUploadController {
    */
   private static getFilesAndFields(request: Request) {
     const uploadedFiles = request.files;
+
     const mapper = (f: globalThis.Express.Multer.File) => ({
       fieldname: f.fieldname,
       originalname: f.originalname,
@@ -75,6 +89,33 @@ export class FileUploadController {
         files.push(...uploadedFiles[filename].map(mapper));
       }
     }
+
+    const upload_to_bucket = (f: globalThis.Express.Multer.File) => {
+      fs.readFile(f.path, (err, data) => {
+        if (err) {
+          throw err;
+        }
+        const params = {
+          Bucket: process.env.AWS_BUCKET_NAME ?? "",
+          Key: f.originalname,
+          Body: data,
+        };
+
+        s3.upload(params, (s3err: Error, _: any) => {
+          if (s3err) throw s3err;
+          console.log(`File ${f.originalname} is uploaded successfully`);
+        });
+      });
+    };
+
+    if (Array.isArray(uploadedFiles)) {
+      uploadedFiles.forEach(upload_to_bucket);
+    } else {
+      for (const filename in uploadedFiles) {
+        uploadedFiles[filename].forEach(upload_to_bucket);
+      }
+    }
+
     return { files, fields: request.body };
   }
 }
